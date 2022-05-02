d225d17aa660890812f0121ec8606aeecde85c81
Junior Alves <helio.alvs@gmail.com>
2022-05-02 16:23:49 -0300

    Config Fauna

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,31 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, useSession, signOut } from 'next-auth/react';
 
 import styles from './styles.module.scss';
 
 export const SignInButton = () => {
-  const isUserLoggedIn = false;
+  const { data: session } = useSession();
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+    >
       <FaGithub color='#04d361'/>
-      Junior Alves
-      <FiX color='#737380' className={styles.closeIcon} />
+      {session.user.name}
+      <FiX 
+        color='#737380'
+        className={styles.closeIcon}
+        onClick={() => signOut()}
+      />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
+    >
       <FaGithub color='#eba417'/>
       Sign in with GitHub
     </button>
